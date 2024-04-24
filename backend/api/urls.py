4d3013bcf02c03bfcac6709c58c2abb665671aea
sacha11 <s.j.g_97@hotmail.com>
2024-04-24 09:36:10 -0300

    adding front-end

---
+++ backend/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("tasks/", views.TaskListCreate.as_view(), name="task-list"),
+    path("tasks/delete/<int:pk>/", views.TaskDelete.as_view(), name="delete-task")
+]
+
+
+
+
---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,25 @@
+import React, { Children, useState } from 'react'
+import { jwtDecode } from 'jwt-decode'
+import { Navigate } from 'react-router-dom'
+import api from '../api'
+import { REFRESH_TOKEN, ACCESS_TOKEN } from '../constants'
+
+const ProtectedRoute = ({ children }) => {
+  const [isAuthorized, setIsAuthorized] = useState(null)
+
+  const refreshToken = async () => {
+
+  }
+
+  const auth = async () => {
+    
+  }
+
+  if (isAuthorized === null) {
+    return <div>Loading..</div>
+  }
+
+  return isAuthorized ? children : <Navigate to="/login"/>
+}
+
+export default ProtectedRoute
